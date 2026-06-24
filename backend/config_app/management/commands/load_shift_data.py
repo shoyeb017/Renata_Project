3fcc9921a14b01ac5b10d164e9e4f20084f4d974
shoyeb017 <shoyebhaque017@gmail.com>
2026-06-24 07:57:18 +0600

    prepare backend deployment

---
+++ backend/config_app/management/commands/load_shift_data.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from config_app.loader import load_data
+
+
+class Command(BaseCommand):
+    help = "Load shift CSV data into database"
+
+    def handle(self, *args, **options):
+        load_data()
+        self.stdout.write(
+            self.style.SUCCESS("Shift data loaded successfully")
+        )
